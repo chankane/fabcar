83c9d70ca30d686e272c481dd1d5654dfe9af7a7
chankane <brawnychocolate@gmail.com>
2019-04-01 10:08:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require("express");
-const app = express();
+var express = require('express');
+var app = express();
 
-const query = require("./query.js");
+var fs = require('fs');
+var https = require('https');
+var query = require('./query.js');
+var options = {
+  key:  fs.readFileSync('./test.key'),
+  cert: fs.readFileSync('./test.cert')
+};
+var server = https.createServer(options,app);
 
-app.get("/", (req, res) => res.send("Hello World"));
-app.get("/y", (req, res) => res.send("Yoshimura"));
-//app.get("/q", (req, res) => res.send(query.getAllCars((res) => console.log(res))));
-app.get("/q", (req, res) => query.getAllCars((resq) => res.send(resq)));
-
-app.listen(80);
-console.log("listen!!");
+app.get('/', function (req, res) {
+    res.end("Hello World.");
+});
+app.get('/query', function (req, res) {
+    query.getAllCars((resq) => res.send(resq));
+});
+server.listen(443);
+console.log("listened...");
